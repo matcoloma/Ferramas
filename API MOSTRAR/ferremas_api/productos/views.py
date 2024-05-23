6ac906e2892e28d3636306ac972e811e80f13d14
Hoxton87 <124192824+Hoxton87@users.noreply.github.com>
2024-05-23 10:25:03 -0400

    Delete API MOSTRAR/ferremas_api directory

--- API MOSTRAR/ferremas_api/productos/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import generics
-from .models import Producto
-from .serializers import ProductoSerializer
-
-class ProductoList(generics.ListAPIView):
-    queryset = Producto.objects.filter(activo=True)
-    serializer_class = ProductoSerializer
